7ff562e88b22cb1f102733bf860b6648bde909b1
Everton Cosmo <everton22cosmo@gmail.com>
2022-06-16 14:15:27 -0300

    inital challenger commit

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -6,20 +6,14 @@ import { Header } from '../../components/Header';
 import { SearchBar } from '../../components/SearchBar';
 import { LoginDataItem } from '../../components/LoginDataItem';
 
-import {
-  Container,
-  Metadata,
-  Title,
-  TotalPassCount,
-  LoginList,
-} from './styles';
+import { Container, Metadata, Title, TotalPassCount, LoginList } from './styles';
 
 interface LoginDataProps {
   id: string;
   service_name: string;
   email: string;
   password: string;
-};
+}
 
 type LoginListDataProps = LoginDataProps[];
 
@@ -30,27 +24,45 @@ export function Home() {
 
   async function loadData() {
     const dataKey = '@savepass:logins';
-    // Get asyncStorage data, use setSearchListData and setData
+    const data = await AsyncStorage.getItem(dataKey);
+
+    if (data) {
+      const dataFormatted = JSON.parse(data);
+      setData(dataFormatted);
+      setSearchListData(dataFormatted);
+    }
   }
 
   function handleFilterLoginData() {
-    // Filter results inside data, save with setSearchListData
+    const dataFiltered = searchListData.filter((data) => {
+      if (data.service_name.includes(searchText)) {
+        return data;
+      }
+    });
+    setSearchListData(dataFiltered);
   }
 
   function handleChangeInputText(text: string) {
     // Update searchText value
+    if (text === '') {
+      setData(searchListData);
+    } else {
+      setSearchText(text);
+    }
   }
 
-  useFocusEffect(useCallback(() => {
-    loadData();
-  }, []));
+  useFocusEffect(
+    useCallback(() => {
+      loadData();
+    }, [])
+  );
 
   return (
     <>
       <Header
         user={{
           name: 'Rocketseat',
-          avatar_url: 'https://i.ibb.co/ZmFHZDM/rocketseat.jpg'
+          avatar_url: 'https://i.ibb.co/ZmFHZDM/rocketseat.jpg',
         }}
       />
       <Container>
@@ -60,17 +72,13 @@ export function Home() {
           value={searchText}
           returnKeyType="search"
           onSubmitEditing={handleFilterLoginData}
-
           onSearchButtonPress={handleFilterLoginData}
         />
 
         <Metadata>
           <Title>Suas senhas</Title>
           <TotalPassCount>
-            {searchListData.length
-              ? `${`${searchListData.length}`.padStart(2, '0')} ao total`
-              : 'Nada a ser exibido'
-            }
+            {searchListData.length ? `${`${searchListData.length}`.padStart(2, '0')} ao total` : 'Nada a ser exibido'}
           </TotalPassCount>
         </Metadata>
 
@@ -78,14 +86,16 @@ export function Home() {
           keyExtractor={(item) => item.id}
           data={searchListData}
           renderItem={({ item: loginData }) => {
-            return <LoginDataItem
-              service_name={loginData.service_name}
-              email={loginData.email}
-              password={loginData.password}
-            />
+            return (
+              <LoginDataItem
+                service_name={loginData.service_name}
+                email={loginData.email}
+                password={loginData.password}
+              />
+            );
           }}
         />
       </Container>
     </>
-  )
+  );
 }
